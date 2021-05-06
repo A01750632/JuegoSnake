d9c689f26c9fe3a2e540b1bcbb01bbad01a4ae3e
DannyAvilaL <a01378664@itesm.mx>
2021-05-06 14:08:21 -0500

    Actualiza el movimiento de la comida de forma aleatoria.

--- snake.py
+++ snake.py
@@ -11,7 +11,7 @@ Exercises
 
 
 from turtle import *
-from random import randrange
+from random import randrange, sample, choice
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -19,7 +19,6 @@ snake = [vector(10, 0)]
 aim = vector(0, -10)
 
 def change(x, y):
-    "Change snake direction."
     aim.x = x
     aim.y = y
 
@@ -28,7 +27,14 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 
 def move():
-    "Move snake forward one segment."
+    """Cambia la dirección de la cabeza de la serpiente
+    Si la cabeza llega a alguno de los límites de la pantalla,
+    entonces será el fin del juego.
+    Cuando las coordenadas de la cabeza y la de la comida coincidan,
+    se incrementará el score del jugador y crecerá la lista del cuerpo.
+    La comida se mueve constantemente de manera aleatoria sin 
+    salirse del borde.
+    """
     head = snake[-1].copy()
     head.move(aim)
 
@@ -51,14 +57,22 @@ def move():
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
-    square(food.x, food.y, 9, 'green')
+    food.move(vector(choice([-10,0,10]), choice([-10,0,10])))
+
+    if not inside(food):        #Si la comida llega a los bordes de la ventana
+        food.x , food.y = 0, 0  #Manda la comida al centro de la ventana.
+
+    square(food.x, food.y, 9, 'green')  # Pinta la nueva posición de la comida
     update()
     ontimer(move, 100)
 
+# Instrucciones que configuran la ventana
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+
+# Al presionar algunas de las flechas, moverá a la serpiente en esa dirección.
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
